aa323c5046d6431bfd1412083935f4cec7ec206e
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-05 20:40:38 +0100

    Creating bookmarks

--- app.rb
+++ app.rb
@@ -12,5 +12,15 @@ class BookmarkManager < Sinatra::Base
     erb(:all_bookmarks)
   end
 
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url])
+    redirect '/bookmarks'
+
+  end
+
   run! if app_file == $0
 end
