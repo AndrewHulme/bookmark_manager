730655ae16618c1e5118293f388b48aaa1b3507c
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-07 20:48:29 +0100

    Wrapping Database Data in Program Objects

--- spec/features/add_bookmarks_spec.rb
+++
@@ -1,9 +0,0 @@
-feature 'add a bookmark' do
-  scenario 'add a bookmark' do
-    visit('/bookmarks/new')
-    fill_in 'url', with: "www.marius.com"
-    click_button "Submit"
-
-    expect(page).to have_content "www.marius.com"
-  end
-end
